74e1d9e0096cdb139b303cfa052d581042e13503
Kevin K <doilerjar@outlook.com>
2018-08-18 06:56:40 +0000

    Done.

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'spec_helper'
 
 describe Show do
@@ -8,6 +9,7 @@ describe Show do
 
   before(:each) do
     @rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
+    binding.pry
     @law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
     @the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
     @fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
